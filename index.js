f8377c7369b91c7061799a4a157099a213cee59d
lostsoup <oliverowen2210@gmail.com>
2021-12-20 19:00:10 -0600

    added UI

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function toTitleCase(str) {
     return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
 }
 
+function update() {
+    winCounter.textContent = wins;
+    lossCounter.textContent = losses;
+}
+
 function computerPlay() {
     // pick random number between 1 through 9, inclusive
     objectSelector = Math.floor(Math.random() * (10 - 1) + 1);
@@ -12,7 +17,7 @@ function computerPlay() {
     } else return "scissors";
 }
 
-function playGame(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if(playerSelection.toLowerCase()=="rock") {
         switch(computerSelection) {
             case "paper": 
@@ -43,27 +48,75 @@ function playGame(playerSelection, computerSelection) {
     } else return false;
 }
 
+function game(select) {
+    if(!(playing)) return;
+    let playerSelection = select;
+    let computerSelection = computerPlay();
+    let result = (playRound(playerSelection, computerSelection));
+    playerInput.textContent = toTitleCase(playerSelection) + '!';
+    computerInput.textContent = toTitleCase(computerSelection) + '!';
 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    while (playerWins < 3 && computerWins < 3) {
-        let playerSelection = prompt("Rock, paper, scissors");
-        let computerSelection = computerPlay();
-        result = (playGame(playerSelection, computerSelection));
-        if(result=="win") {
-            console.log(`You win! ${toTitleCase(playerSelection)} beats ${computerSelection}.`)
-            playerWins++;
-        } else if(result=="lose") {
-            console.log(`You lose! ${toTitleCase(computerSelection)} beats ${playerSelection}.`)
-            computerWins++;
-        } else if(result=="tie") {
-            console.log("It's a tie.");
-        } else console.log("Invalid input.");
-        console.log(playerWins, computerWins);   
-    }     
+    if(result=="win") {
+        resultMessage.textContent = `${toTitleCase(playerSelection)} beats ${computerSelection}.`;
+        wins++;
+        playerInput.classList.add('green');
+        playerInput.classList.remove('red');
+        computerInput.classList.add('red');
+        computerInput.classList.remove('green');
+    } else if(result=="lose") {
+        resultMessage.textContent = `${toTitleCase(computerSelection)} beats ${playerSelection}.`;
+        losses++;
+        playerInput.classList.add('red')
+        playerInput.classList.remove('green');
+        computerInput.classList.add('green')
+        computerInput.classList.remove('red');
+    } else if(result=="tie") {
+        resultMessage.textContent = "It's a tie.";
+        inputs.classList.remove('red', 'green')
+    } else resultMessage.textContent = "Invalid input.";
+    update();
+    endGame();
+};    
 
-    if(playerWins > computerWins) {
-        return true;
-    } else return false;
+function endGame() {
+    if(wins == maxGames) {
+        message.textContent = 'Congratulations! You win!';
+        message.classList.add('green');
+        for(const input of inputs) input.classList.remove('green', 'red');
+        buttonsDiv.parentNode.removeChild(buttonsDiv);
+        playing = false;
+    }  else if(losses == maxGames) {
+        message.textContent = 'You lose. Better luck next time!';
+        message.classList.add('red');
+        for(const input of inputs) input.classList.remove('green', 'red');
+        buttonsDiv.parentNode.removeChild(buttonsDiv);
+        playing = false;
+    }
 }
+
+
+
+winCounter = document.querySelector('#wins');
+lossCounter = document.querySelector('#losses');
+
+playerInput = document.querySelector('#playerInput');
+computerInput = document.querySelector('#computerInput');
+inputs = [playerInput, computerInput];
+
+resultMessage = document.querySelector('#message');
+
+rock = document.querySelector('#rock');
+paper = document.querySelector('#paper');
+scissors = document.querySelector('#scissors');
+
+let maxGames = 5;
+let wins = 0;
+let losses = 0;
+let playing = true;
+
+rock.addEventListener('click', (e) => game('rock'));
+paper.addEventListener('click', (e) => game('paper'));
+scissors.addEventListener('click', (e) => game('scissors'));
+btns = [rock, paper, scissors];
+
+buttonsDiv = document.querySelector('#buttons');
